ce5d0524070dc1ec5d1356a42eecb820c70cb904
Didoshotev <didoshotev@gmail.com>
2020-12-10 16:44:42 +0200

    fix what not logged users can seee

--- src/app/post/post-list/post-item/post-item.component.ts
+++ src/app/post/post-list/post-item/post-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Input } from '@angular/core'
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-post-item',
@@ -7,12 +8,16 @@ import { Input } from '@angular/core'
   styleUrls: ['./post-item.component.css']
 })
 export class PostItemComponent implements OnInit {
+  isLogged;
   @Input() postItem; 
   @Input() index;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.user.subscribe(data => {
+      this.isLogged = !!data;
+    })
   }
 
   onReadMore(){
